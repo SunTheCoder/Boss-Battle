d50b0ecdf1da0075982dd7473a0eb4c8be2e8660
SunTheCoder <sunenglishjr@gmail.com>
2024-04-25 09:26:55 -0400

    commit

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-const { boss1 } = require('./characters')
-const { player1 } = require('./characters')
 const { playGame } = require('./playgame')
 
     playGame()
-   
-    module.exports = {
-        
-        player1,
-        boss1
-    }
